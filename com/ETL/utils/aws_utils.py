a6fbbcd813d956326012296ecd0e9be1623fff81
hadka <hadkark22112002@gmail.com>
2024-10-30 22:53:03 +0530

    commit_1

--- com/ETL/utils/aws_utils.py
+++ com/ETL/utils/aws_utils.py
@@ -39,14 +39,15 @@ def read_from_mysql(spark, table_name, part_col, secret_conf):
 
 def read_from_sftp(spark, app_secret, filepath, secret_file, src_conf):
     return spark.read \
-        .format("com.springml.spark.sftp")\
-        .option("host", app_secret["sftp_conf"]["hostname"])\
-        .option("port", app_secret["sftp_conf"]["port"])\
-        .option("username", app_secret["sftp_conf"]["username"])\
-        .option("pem", secret_file \
-        .option("fileType", src_conf["OL"]["sftp_conf"]["csv"]) \
+        .format("com.springml.spark.sftp") \
+        .option("host", app_secret["sftp_conf"]["hostname"]) \
+        .option("port", app_secret["sftp_conf"]["port"]) \
+        .option("username", app_secret["sftp_conf"]["username"]) \
+        .option("pem", secret_file) \
+        .option("fileType", src_conf["sftp_conf"]["filetype"]) \
         .option("delimiter", "|") \
         .load(filepath)
+
 
 
 
