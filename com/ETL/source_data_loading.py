dc9c2d8369c6034e2b9150036102935a1141f209
hadka <hadkark22112002@gmail.com>
2024-10-30 22:56:08 +0530

    commit_1

--- com/ETL/source_data_loading.py
+++ com/ETL/source_data_loading.py
@@ -58,7 +58,8 @@ if __name__ == '__main__':
             txn_df = ut.read_from_sftp(spark,
                                        app_secret,
                                        os.path.abspath(current_dir + "/../" + app_secret["sftp_conf"]["pem"]),
-                                       src_conf["sftp_conf"]["directory"] + "/receipts_delta_GBR_14_10_2017.csv")
+                                       src_conf["sftp_conf"]["directory"] + "/receipts_delta_GBR_14_10_2017.csv"),
+                                       src_conf
             txn_df = txn_df.withColumn("ins_dt", current_date())
             txn_df.show(5, False)
             # write data to S3 in parquet format
