af9f70c726e9b718963f7ac47042fa91ca7d23eb
hadka <hadkark22112002@gmail.com>
2024-11-03 20:28:08 +0530

    commit_1

--- com/ETL/source_data_loading.py
+++ com/ETL/source_data_loading.py
@@ -64,8 +64,7 @@ if __name__ == '__main__':
                 app_secret,
                 pem_file_path,
                 os.path.abspath(current_dir + "/../" + app_secret["sftp_conf"]["pem"]),
-                src_conf["sftp_conf"]["directory"] + "/receipts_delta_GBR_14_10_2017.csv",
-                src_conf  # Ensure this line is properly aligned
+                src_conf["sftp_conf"]["directory"] + "/receipts_delta_GBR_14_10_2017.csv"
             )
             txn_df = txn_df.withColumn("ins_dt", current_date())
             txn_df.show(5, False)
