4ffc2b9e70102d6a286cdcce7ec11795733fb2b5
hadka <hadkark22112002@gmail.com>
2024-11-05 20:16:14 +0530

    commit_1

--- com/ETL/source_data_loading.py
+++ com/ETL/source_data_loading.py
@@ -66,7 +66,6 @@ if __name__ == '__main__':
                 spark,
                 app_secret,
                 os.path.abspath(current_dir + "/../../" + app_secret["sftp_conf"]["pem"]),
-                src_conf["sftp_conf"]["directory"] + "/receipts_delta_GBR_14_10_2017.csv",
                 file_path
             )
 
